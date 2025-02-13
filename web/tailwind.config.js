db0af7d078f8b2016e88f93b7b72cc225d38c818
Emanuel Vinícius <evtsg@cin.ufpe.br>
2025-02-12 23:54:59 -0300

    commit till half of lesson 3

---
+++ web/tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090A'
+      },
+
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        zoomIn: {
+          '0%': { opacity: 0, transform: 'translate(-50%, -50%) scale(0.95)' },
+          '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+        },
+        zoomOut: {
+          '0%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+          '100%': { opacity: 0, transform: 'translate(-50%, -50%) scale(0.95)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-out',
+        fadeOut: 'fadeOut 0.2s ease-in',
+        zoomIn: 'zoomIn 0.3s ease-out',
+        zoomOut: 'zoomOut 0.2s ease-in',
+      },
+    },
+  },
+  plugins: [],
+}
+
