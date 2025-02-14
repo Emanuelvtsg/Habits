88fe2253f1240bc501d11d02b481675f600511e8
Emanuel Vinícius <evtsg@cin.ufpe.br>
2025-02-13 23:46:19 -0300

    adding lesson 3 mobile content

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -5,13 +5,20 @@ import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-yea
 
 import { Header } from "../components/Header";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { useNavigation } from "@react-navigation/native";
+import dayjs from "dayjs";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesfromYearStart = generateDatesFromYearBeginning();
 const minimumSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSize - datesfromYearStart.length;
 
+const firstDayOfYear = dayjs().startOf('year')
+const amountOfDaysTillFirstDayOfYear =  firstDayOfYear.day()
+
 export function Home(){
+
+    const { navigate } = useNavigation();
 
     return (
         <View className="flex-1 bg-background px-8 pt-16">
@@ -38,10 +45,23 @@ export function Home(){
             >
                 <View className="flex-row flex-wrap">
 
-                    {
+                     {
+                        amountOfDaysTillFirstDayOfYear > 0 && Array
+                        .from({ length: amountOfDaysTillFirstDayOfYear })
+                        .map((_, index) => (
+                            <View
+                                key={index}
+                                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                style={{width: DAY_SIZE, height: DAY_SIZE}}
+                            />
+                        ))
+                    }
+
+                    {   
                         datesfromYearStart.map(date =>(
                             <HabitDay
                                 key={date.toISOString()}
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
                             />
                         ))
                     }
